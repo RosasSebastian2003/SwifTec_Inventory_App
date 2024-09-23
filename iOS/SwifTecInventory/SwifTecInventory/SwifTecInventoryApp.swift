48fe03e64cba4fd5c66c435cb4ba37f093ed1758
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-09-23 15:51:41 -0600

    done

---
+++ iOS/SwifTecInventory/SwifTecInventory/SwifTecInventoryApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwifTecInventoryApp.swift
+//  SwifTecInventory
+//
+//  Created by Sebastian Rosas Maciel on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwifTecInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ iOS/SwifTecInventory/SwifTecInventory/Views/InventoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  InventoryListView.swift
+//  SwifTec-Inventory-App
+//
+//  Created by Sebastian Rosas Maciel on 9/22/24.
+//
+
+import SwiftUI
+
+struct InventoryListView: View {
+    @State var viewModel = InventoryListViewModel()
+    
+    var body: some View {
+        Text("test text")
+    }
+}
+
+#Preview {
+    InventoryListView()
+}
