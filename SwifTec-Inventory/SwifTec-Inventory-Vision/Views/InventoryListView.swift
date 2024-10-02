fa0ebbadc01062984345372678fbbe38d1e530ff
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-10-02 08:45:49 -0600

    Done

--- SwifTec-Inventory/SwifTec-Inventory-Vision/Views/InventoryListView.swift
+++ SwifTec-Inventory/SwifTec-Inventory-Vision/Views/InventoryListView.swift
@@ -10,35 +10,4 @@ import RealityKit
 
 struct InventoryListView: View {
     
-    @State var vm = InventoryListViewModel()
-    private let gridItems: [GridItem] =
-    [.init(.adaptive(minimum: 240), spacing: 16)]
-    
-    var body: some View {
-        ScrollView {
-            LazyVGrid(columns: gridItems) {
-                ForEach(vm.items) { item in
-                    InventoryTrackerItemView(item: item)
-                        .onDrag {
-                            guard let usdzURL = item.usdzURL else { return NSItemProvider() }
-                            return NSItemProvider(object: USDZItemProvider(usdzURL: usdzURL))
-                        }
-                }
-            }
-            .padding(.vertical)
-            .padding(.horizontal, 30)
-        }
-        .navigationTitle("AR Inventory")
-        .onAppear { vm.fetchItems() }
-    }
-}
-
-#Preview {
-    
-    @StateObject var navVM = NavigationViewModel()
-    
-    return NavigationStack {
-        InventoryListView()
-            .environmentObject(navVM)
-    }
 }
