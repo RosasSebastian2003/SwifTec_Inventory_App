2e8e942b9685f5de9a159eafd3d14b3b025908ec
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-10-02 07:02:05 -0600

    missing changes

--- iOS/SwifTecInventory/SwifTecInventory/View Models/Sample VM.swift
+++ iOS/SwifTecInventory/SwifTecInventory/View Models/Sample VM.swift
@@ -5,28 +5,3 @@
 //  Created by Eugenio Pedraza on 9/25/24.
 //
 
-import Foundation
-import FirebaseFirestore
-import SwiftUI
-import QuickLookThumbnailing
-
-@Observable
-class SampleVM: ObservableObject {
-    let db = Firestore.firestore()
-    let formType: FormType
-    
-    let id: String
-    var name: String
-    var quantity: Int = 0
-    var thumbnailURL: URL?
-    
-    var loadingState: LoadingType = .none
-    var error: Error?
-    
-    var uploadingState: LoadingType = .none
-    var showUSDZSource: Bool = false
-    
-    
-}
-
-
